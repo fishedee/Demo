fdf886dd7bc16f4ccefb0c456391e6d5be786cae
fishedee <306766045@qq.com>
2018-06-07 23:04:23 +0800

    加入vue的hello world

---
+++ vue/basic/src/app.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div class="message">{{ msg }}</div>
+		<button v-on:click="go">点我</button>
+	</div>
+</template>
+
+<script>
+const delay = (timeout)=>{
+	return new Promise((resolve,reject)=>{
+		setTimeout(resolve,timeout);
+	});
+}
+
+export default {
+	data () {
+		return {
+			msg: 'Hello World!'
+		}
+	},
+	methods:{
+		async go(){
+			await delay(1000);
+			this.msg = 'Hello Vue!';
+		}
+	}
+}
+</script>
+
+<style>
+.message {
+ color: blue;
+}
+button{
+	padding:10px;
+	font-size:16px;
+}
+</style>
