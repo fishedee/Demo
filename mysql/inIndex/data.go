737b0e5ffbb0fd9a525c11c80007cc4bf4884c65
fishedee <306766045@qq.com>
2019-06-19 23:43:43 +0800

    更新数据量

--- mysql/inIndex/data.go
+++ mysql/inIndex/data.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	. "github.com/fishedee/app/database"
 	. "github.com/fishedee/language"
+	"io/ioutil"
 	"math/rand"
-	"time"
+	"os"
 )
 
-type Remind1 struct {
+type Remind struct {
 	RemindId        int `xorm:"autoincr"`
 	ReceiveClientId int
 	SendClientId    int
@@ -18,30 +19,6 @@ type Remind1 struct {
 	IsRead          int
 	ContentId       int
 	ContentType     int
-	CreateTime      time.Time `xorm:"created"`
-	ModifyTime      time.Time `xorm:"updated"`
-}
-
-func (this Remind1) TableName() string {
-	return "t_remind_1"
-}
-
-type Remind2 struct {
-	RemindId        int `xorm:"autoincr"`
-	ReceiveClientId int
-	SendClientId    int
-	Text            string
-	Id              int
-	RemindType      int
-	IsRead          int
-	ContentId       int
-	ContentType     int
-	CreateTime      time.Time `xorm:"created"`
-	ModifyTime      time.Time `xorm:"updated"`
-}
-
-func (this Remind2) TableName() string {
-	return "t_remind_2"
 }
 
 func randomInt() int {
@@ -65,48 +42,58 @@ func randomString(length int) string {
 	return string(result)
 }
 
+func insertRemind(str *bytes.Buffer, tableName string, reminds []Remind) {
+	str.WriteString("insert into " + tableName + "(receiveClientId,sendClientId,text,id,remindType,isRead,contentId,contentType) values\n")
+	for index, remind := range reminds {
+		str.WriteString(fmt.Sprintf("(%v,%v,'%v',%v,%v,%v,%v,%v)",
+			remind.ReceiveClientId,
+			remind.SendClientId,
+			remind.Text,
+			remind.Id,
+			remind.RemindType,
+			remind.IsRead,
+			remind.ContentId,
+			remind.ContentType))
+		if index == len(reminds)-1 {
+			str.WriteString(";\n")
+		} else {
+			str.WriteString(",\n")
+		}
+	}
+}
+
 func main() {
 	defer CatchCrash(func(e Exception) {
 		fmt.Printf("fail %v\n", e.Error())
 	})
-	db, err := NewDatabase(DatabaseConfig{
-		Driver:   "mysql",
-		Host:     "localhost",
-		Port:     3306,
-		User:     "root",
-		Passowrd: "1",
-		Database: "MySqlTest",
-		Charset:  "utf8mb4",
-	})
-	if err != nil {
-		panic(err)
-	}
+	builder := bytes.Buffer{}
+	builder.WriteString("use MySqlTest;\n\n")
+	builder.WriteString("set global max_allowed_packet = 100 * 1024 * 1024;\n\n")
 
 	//普通的数据，模拟实际的数据分布
-	for i := 0; i != 1000; i++ {
-		reminds := []Remind1{}
-		for j := 0; j != 1000; j++ {
-			reminds = append(reminds, Remind1{
-				ReceiveClientId: randomInt() % 10000,
-				RemindType:      randomInt() % 10,
-				ContentType:     randomInt() % 10,
-			})
-		}
-		db.MustInsert(reminds)
+	remind1s := []Remind{}
+	remindCount := 3000000
+	for i := 0; i != remindCount; i++ {
+		remind1s = append(remind1s, Remind{
+			ReceiveClientId: randomInt() % 10000,
+			RemindType:      randomInt() % 10,
+			ContentType:     randomInt() % 5,
+		})
 	}
+	insertRemind(&builder, "t_remind_1", remind1s)
 
 	//更一般的数据
-	for i := 0; i != 1000; i++ {
-		reminds := []Remind2{}
-		for j := 0; j != 1000; j++ {
-			reminds = append(reminds, Remind2{
-				ReceiveClientId: randomInt() % 10000,
-				RemindType:      randomInt() % 1000,
-				ContentType:     randomInt() % 1000,
-			})
-		}
-		db.MustInsert(reminds)
+	remind2s := []Remind{}
+	for i := 0; i != remindCount; i++ {
+		remind2s = append(remind2s, Remind{
+			ReceiveClientId: randomInt() % 3,
+			RemindType:      randomInt() % 7,
+			ContentType:     randomInt() % 5,
+		})
 	}
+	insertRemind(&builder, "t_remind_2", remind2s)
 
+	os.Remove("data.sql")
+	ioutil.WriteFile("data.sql", builder.Bytes(), os.ModePerm)
 	fmt.Println("finish!")
 }
--- mysql/inIndex/query.sql
+++ mysql/inIndex/query.sql
@@ -5,3 +5,10 @@ use MySqlTest;
 
 explain select * from t_remind_1 where receiveClientId = 5234 and (remindType in (3,6,2)) and ( contentType in (2,4)) order by remindId desc limit 10\G;
 select * from t_remind_1 where receiveClientId = 5234 and (remindType in (3,6,2)) and ( contentType in (2,4)) order by remindId desc limit 10;
+
+# 查询2
+explain select * from t_remind_2 force index(recvAndIsRAndReTypeIndex) where receiveClientId = 2 and (remindType in (3,6,2)) and ( contentType in (2,4)) order by remindId desc limit 10\G;
+select * from t_remind_2 force index(recvAndIsRAndReTypeIndex) where receiveClientId = 2 and (remindType in (3,6,2)) and ( contentType in (2,4)) order by remindId desc limit 10;
+
+explain select * from t_remind_2 force index(primary) where receiveClientId = 2 and (remindType in (3,6,2)) and ( contentType in (2,4)) order by remindId desc limit 10\G;
+select * from t_remind_2 force index(primary) where receiveClientId = 2 and (remindType in (3,6,2)) and ( contentType in (2,4)) order by remindId desc limit 10;
