b01d2347bfc1c50d83421da58647a19d47d94dca
fishedee <306766045@qq.com>
2019-07-27 22:38:11 +0800

    加入类型

---
+++ rust/type/main.rs
@@ -0,0 +1,48 @@
+fn basic(){
+	//布尔
+	let a = true;
+	let b:bool = !a;
+	println!("a = {},b = {}",a,b);
+
+	//整数
+	let c = 32;
+	let d:i32 = 34;
+	let e = c +d;
+	println!("c={},d={},c+d={}",c,d,e);
+
+	//浮点数
+	let f:f32 = 123.45;
+	println!("f = {}",f);
+
+	//字符
+	let g = 'k';
+	println!("g = {}",g);
+}
+
+fn string(){
+	let x:&str = "Hello Fish";
+	let y:String = String::from("Hello Cat");
+	println!("x = {},y = {}",x,y);
+}
+
+fn array(){
+	let x = [1,2,3];
+	let x2:[i32;4] = [4,5,6,7];
+	let x3:Vec<i32> = vec![8,9,10,11,12];
+	//println!("x={},x2={}",x,x2)
+	//println!("x3={}",x3)
+	println!("x len={},x2 len={},x3 len={}",x.len(),x2.len(),x3.len())
+}
+
+fn tunple(){
+	let x = (1,'c');
+	let y:(i32,char) = (2,'d');
+	println!("x.0={},y.1={}",x.0,y.1);
+}
+
+fn main(){
+	basic();
+	string();
+	array();
+	tunple();
+}
