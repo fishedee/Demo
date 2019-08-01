500a516e11e5ae9e8105923681a29527494f45d2
fishedee <306766045@qq.com>
2019-08-01 22:17:41 +0800

    加入map

--- rust/type/main.rs
+++ rust/type/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn basic(){
 	//布尔
 	let a = true;
@@ -26,17 +28,34 @@ fn show2(data :&str){
 fn string(){
 	let x:&str = "Hello Fish";
 	let mut y:String = String::from("Hello Cat");
-	println!("x = {},y = {}",x,y);
+	let mut z :  String = x.to_string();
+	println!("x = {},y = {},z = {}",x,y,z);
 
+	//拼接
 	y.push_str(" And Dog");
-	println!("y = {}",y);
+	z = z + "_" + &y; 
+	println!("y = {},z={}",y,z);
 
-	let z = y.clone();
-	println!("z = {}",z);
+	//复制
+	let m = y.clone();
+	println!("m = {}",m);
 
 	show2(x);
 	show2(&y[0..3]);
-	show2(&z[3..]);
+	show2(&m[3..]);
+
+	//格式化
+	let n = format!("y=[{}],z=[{}]",y,z);
+	println!("{}",n);
+
+	//遍历
+	let l = String::from("a你好c");
+	for c in l.chars(){
+		println!("char = {}",c)
+	}
+	for d in l.bytes(){
+		println!("byte = {}",d)
+	}
 }
 
 fn show(data:&[i32]){
@@ -59,8 +78,21 @@ fn vec(){
 	//println!("x3={}",x3)
 	println!("x3 len={}",x3.len());
 
-	//添加
+	//添加和删除
 	x3.push(13);
+	x3.pop();
+
+	//下标操作
+	{
+		let third :& mut i32 = & mut x3[2];
+		*third = 34;
+	}
+	{
+		match x3.get(102){
+			Some(data)=>println!("{}",data),
+			None=>println!("none! 102 index"),
+		}
+	}
 
 	println!("x3 {:?}",x3);
 
@@ -69,10 +101,58 @@ fn vec(){
 		println!("x3 {}",x)
 	}
 
+	//iterator可变遍历
+	{
+		for x in & mut x3{
+			*x += 100;
+		}
+	}
+
 	//index遍历
 	for x in 0..x3.len(){
 		println!("x3 {}",x3[x])
 	}
+}
+
+fn map(){
+	let mut scores = HashMap::new();
+
+	//添加
+	scores.insert(String::from("Blue"),10);
+	scores.insert(String::from("Green"),50);
+
+	println!("scores {:?}",scores);
+
+	//查找
+	let score1 = scores.get(&String::from("Yellow"));
+	let score2 = scores.get(&String::from("Blue"));
+
+	if let Some(v) = score1{
+		println!("score1 value {}",v);
+	}else{
+		println!("score1 empty!");
+	}
+
+	if let Some(v) = score2{
+		println!("score2 value {}",v);
+	}else{
+		println!("score2 empty!");
+	}
+
+	//不存在就添加，总是返回引用
+	word_count();
+}
+
+fn word_count(){
+	let text = "hello world wonderful world , hello fish , you will enjoy";
+
+	let mut map = HashMap::new();
+
+	for word in text.split_whitespace(){
+		let count = map.entry(word).or_insert(0);
+		*count += 1;
+	}
+	println!("{:?}",map)
 }
 
 fn tunple(){
@@ -87,4 +167,5 @@ fn main(){
 	array();
 	vec();
 	tunple();
+	map();
 }
