2e41a0f309ff918b1a11cfd3c0e6e9ce61139f7d
fishedee <306766045@qq.com>
2019-08-02 19:43:57 +0800

    加入test

--- rust/test/adder/target/debug/adder-d17a4554583202d5.d
+++
@@ -1,1 +0,0 @@
-/Users/fish/Project/Demo/rust/test/adder/target/debug/adder-d17a4554583202d5: /Users/fish/Project/Demo/rust/test/adder/src/lib.rs
--- rust/test/adder/target/debug/deps/adder-7ad2099aa627e844.d
+++
@@ -1,7 +0,0 @@
-/Users/fish/Project/Demo/rust/test/adder/target/debug/deps/adder-7ad2099aa627e844.rmeta: src/lib.rs
-
-/Users/fish/Project/Demo/rust/test/adder/target/debug/deps/libadder-7ad2099aa627e844.rlib: src/lib.rs
-
-/Users/fish/Project/Demo/rust/test/adder/target/debug/deps/adder-7ad2099aa627e844.d: src/lib.rs
-
-src/lib.rs:
