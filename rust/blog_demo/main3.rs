7ef1852e2fcf0b11d12b028a02b95248464c5763
fishedee <306766045@qq.com>
2019-08-13 21:13:18 +0800

    更新代码

---
+++ rust/blog_demo/main3.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+struct Class{
+	class_id:i32,
+	name:String,
+}
+
+#[derive(Debug)]
+struct User<'a>{
+	user_id:i32,
+	age:i32,
+	class1:&'a Class,
+	class2:&'a Class,
+}
+
+fn main(){//作用域'a
+   
+    let class_one = Class{class_id:123,name:"mc".to_string()};
+    let class_ref:&Class;
+    {	
+    	let class_two = Class{class_id:789,name:"md".to_string()};
+    	let user = User{user_id:234,age:789,class1:&class_one,class2:&class_two};
+
+    	class_ref = user.class1;
+    	println!("{:?}",class_ref);
+    }
+
+    println!("{:?}",class_ref);
+}
