9830c1b4d7030685987359edde75e4091a07e5ab
fishedee <306766045@qq.com>
2019-08-13 22:12:47 +0800

    结构体的作用域是静态的

--- rust/blog_demo/main.rs
+++ rust/blog_demo/main.rs
@@ -12,18 +12,29 @@ struct User<'a>{
 	class2:&'a Class,
 }
 
+impl<'a> User<'a>{
+	fn set_class1(&mut self,class:&'a Class){
+		self.class1 = class;
+	}
+}
+
 fn main(){//作用域'a
     
     let class_one = Class{class_id:789,name:"md".to_string()};
-    let mut user = User{user_id:234,age:789,class1:&class_one,class2:&class_one};
-    {	
-    	let class_two = Class{class_id:789,name:"md".to_string()};
-    	user.class1 = &class_two;
+    let class_ref:&Class;
+    {
+    	let class_two = Class{class_id:123,name:"me".to_string()};
+    	let mut user = User{user_id:234,age:789,class1:&class_two,class2:&class_two};
 
-        println!("{:?}",user);
+    	println!("{:?}",user);
 
-        user.class1 = &class_one;
+    	user.class1 = &class_one;
+    	user.class2 = &class_one;
+
+    	println!("{:?}",user);
+
+    	class_ref = user.class1;
     }
 
-    println!("{:?}",user);
+    println!("{:?}",class_ref);
 }
---
+++ rust/blog_demo/main5.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+struct Class{
+	class_id:i32,
+	name:String,
+}
+
+#[derive(Debug)]
+struct User<'a>{
+	user_id:i32,
+	age:i32,
+	class1:&'a Class,
+	class2:&'a Class,
+}
+
+fn main(){//作用域'a
+    
+    let class_one = Class{class_id:789,name:"md".to_string()};
+    
+    {	
+    	let mut user = User{user_id:234,age:789,class1:&class_one,class2:&class_one};
+    	let class_two = Class{class_id:789,name:"md".to_string()};
+    	user.class1 = &class_two;
+
+        println!("{:?}",user);
+
+        user.class1 = &class_one;
+
+        
+
+    println!("{:?}",user);
+    }
+}
