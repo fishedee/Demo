f614d4a9cf7783141571767ccca533642b24ce2f
fishedee <306766045@qq.com>
2019-08-14 17:42:18 +0800

    更新rust

---
+++ rust/blog_demo/main6.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct Class{
+	class_id:i32,
+	name:String,
+}
+
+#[derive(Debug)]
+struct User<'a>{
+	user_id:i32,
+	age:i32,
+	class1:&'a Class,
+	class2:&'a Class,
+}
+
+impl<'a> User<'a>{
+	fn set_class1(&mut self,class:&'a Class){
+		self.class1 = class;
+	}
+}
+
+fn main(){//作用域'a
+    
+    let class_one = Class{class_id:789,name:"md".to_string()};
+    let class_ref:&Class;
+    {
+    	let class_two = Class{class_id:123,name:"me".to_string()};
+    	let mut user = User{user_id:234,age:789,class1:&class_two,class2:&class_two};
+
+    	println!("{:?}",user);
+
+    	//user.class1 = &class_one;
+    	//user.class2 = &class_one;
+
+    	let user2 = User{user_id:234,age:789,class1:&class_one,class2:&class_one};
+
+    	println!("{:?}",user2);
+
+    	class_ref = user2.class1;
+    }
+
+    println!("{:?}",class_ref);
+}
