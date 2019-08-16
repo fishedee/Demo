0dc03a33d886a112f29b5a2daefb9cc28a85e936
fishedee <306766045@qq.com>
2019-08-16 21:27:45 +0800

    加入async的demo

---
+++ rust_async/1_basic/src/main.rs
@@ -0,0 +1,10 @@
+use futures::executor::block_on;
+
+async fn hello_world() {
+    println!("hello, world!");
+}
+
+fn main() {
+    let future = hello_world(); // Nothing is printed
+    block_on(future); // `future` is run and "hello, world!" is printed
+}
