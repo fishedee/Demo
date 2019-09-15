3c5ffe54626cb5a32191c013fd214036a249f67f
fishedee <306766045@qq.com>
2019-09-15 09:14:21 +0800

    更新

--- grafana/1_install/linux.sh
+++ grafana/1_install/linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://dl.grafana.com/oss/release/grafana_6.3.5_amd64.deb
-sudo dpkg -i grafana_5.1.3_amd64.deb
+sudo dpkg -i grafana_6.3.5_amd64.deb
 sudo service grafana-server start
