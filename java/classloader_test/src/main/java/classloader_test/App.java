f74e64e4a5d41df36b0f21b56ab7561954b44665
fishedee <306766045@qq.com>
2021-02-10 21:54:40 +0800

    加入资源文件加载

--- java/classloader_test/src/main/java/classloader_test/App.java
+++ java/classloader_test/src/main/java/classloader_test/App.java
@@ -1,6 +1,9 @@
 package classloader_test;
 
 import java.lang.reflect.Method;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Hello world!
@@ -20,9 +23,37 @@ public class App
             Object obj = clazz.newInstance();
             Object result = method.invoke(obj,1,2);
             System.out.println(result);
+
+            //打印classLoader树
+            System.out.println("begin --- classLoader trace ---");
+            ClassLoader classLoader = loader;
+            do{
+                System.out.println(classLoader);
+                classLoader = classLoader.getParent();
+            }while(classLoader!= null);
+            System.out.println("end --- classLoader trace ---");
+
+            //设置线程变量上的classLoader
+            classLoader = Thread.currentThread().getContextClassLoader();
+            System.out.println("default threadClassLoader: "+classLoader);
+
+            Thread.currentThread().setContextClassLoader(loader);
+            classLoader = Thread.currentThread().getContextClassLoader();
+            System.out.println("setAfter threadClassLoader: "+classLoader);
+
+            //根据classLoader获取资源
+            InputStream stream = loader.getResourceAsStream("abc.xml");
+            System.out.println(stream);
+            System.out.println(App.getStreamData(stream));
         }catch(Exception e){
             e.printStackTrace();
         }
 
+    }
+
+    public static String getStreamData( InputStream in) throws IOException{
+        byte[] result = new byte[in.available()];
+        in.read(result);
+        return new String(result,StandardCharsets.UTF_8);
     }
 }
