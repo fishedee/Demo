681935a5623dbf39ca46dfe897a281c452bdcc82
balefcloud <9228537+balefcloud@user.noreply.gitee.com>
2021-07-02 11:19:49 +0800

    加入generic

--- typescript/generic/src/binder.ts
+++ typescript/generic/src/binder.ts
@@ -1,43 +1,25 @@
 //函数重载
 type CombineType<U> = Array<U> | Object | number 
-
-export default function createBinder<T extends unknown[]> (data:T):ArrayBinder<T>;
-export default function createBinder<T extends object> (data:T):ObjectBinder<T>;
-export default function createBinder<T extends number> (data:T):NumberBinder;
-export default function createBinder<T extends string> (data:T):StringBinder;
+type NumberBinderChecker<T> = T extends number? NumberBinder:never;
+type StringBinderChecker<T> = T extends string? StringBinder:never;
+type ObjectBinderChecker<T> = T extends object ? (T extends unknown[] ? ArrayBinder<T>:ObjectBinder<T>):never;
+type AllBinderChecker<T> = NumberBinderChecker<T> | StringBinderChecker<T> | ObjectBinderChecker<T>;
 
 //这样能做多个复杂函数的重载
-export default function createBinder<T>(data:T) {
-    if( data instanceof Array ){
-        return createBinderWithPath(data,[]);
-    }else if( typeof data === 'object'){
-        return createBinderWithPath(data as unknown as object,[]);
-    }else if( typeof data === 'number'){
-        return createBinderWithPath(data,[]);
-    }else if( typeof data === 'string'){
-        return createBinderWithPath(data,[]);
-    }else{
-        throw new Error("123");
-    }
+export default function createBinder<T>(data:T):AllBinderChecker<T>{
+    return createBinderWithPath(data,[]);
 }
 
-
-function createBinderWithPath<T extends unknown[]> (data2:T,path:string[]):ArrayBinder<T>;
-function createBinderWithPath<T extends object> (data2:T,path:string[]):ObjectBinder<T>;
-function createBinderWithPath<T extends number> (data2:T,path:string[]):NumberBinder;
-function createBinderWithPath<T extends string> (data2:T,path:string[]):StringBinder;
-
-
 //函数重载的实现，通过instanceOf选择不同的类型
-function createBinderWithPath<T>(data2:T,path:string[]){
-    if( data2 instanceof Array){
-        return new ArrayBinder(data2,path);
-    }else if( typeof data2 === 'object'){
-        return new ObjectBinder(data2,path);
-    }else if( typeof data2 === 'number'){
-        return new NumberBinder(data2,path);
-    }else if( typeof data2 === 'string'){
-        return new StringBinder(data2,path);
+function createBinderWithPath<T>(data:T,path:string[]):AllBinderChecker<T>{
+    if( data instanceof Array){
+        return new ArrayBinder(data,path) as AllBinderChecker<T>;
+    }else if( typeof data === 'object'){
+        return new ObjectBinder(data as unknown as object,path) as AllBinderChecker<T>;
+    }else if( typeof data === 'number'){
+        return new NumberBinder(data,path) as AllBinderChecker<T>;
+    }else if( typeof data === 'string'){
+        return new StringBinder(data,path) as AllBinderChecker<T>;
     }else{
         throw new Error("123");
     }
@@ -83,36 +65,22 @@ class ObjectBinder<T extends object> extends Binder<T>{
         super(data,path);
     }
 
-    public key<K extends keyof T,U extends T[K]>(index:K):ObjectBinder<U>;
-    public key<K extends keyof T>(index:K):ArrayBinder<U>;
-    public key<K extends keyof T>(index:K):NumberBinder;
-    public key<K extends keyof T>(index:K):String; 
-    public key<K extends keyof T,U extends T[K]>(index:K){
-        let value = this.data[index];
-        //这里不能再继续传递createBinderWithPath，ts推导不了多一层的泛型
-        if( typeof value === 'object' && value instanceof Array){
-            return new ArrayBinder(value,[...this.path,index+""]);
-        }else if( typeof value === 'object'){
-            return new ObjectBinder(value as unknown as object,[...this.path,index+""]);
-        }else if( typeof value === 'number'){
-            return new NumberBinder(value,[...this.path,index+""]);
-        }else if( typeof value === 'string'){
-            return new StringBinder(value,[...this.path,index+""]);
-        }else{
-            throw new Error("123");
-        }
+    public key<K extends keyof T>(index:K):AllBinderChecker<T[K]>{
+        return createBinderWithPath(this.data[index],[...this.path,index+""]);
     }
 }
     
-class ArrayBinder<T extends unknown[]> extends Binder<Array<T>>{
+type ElementType<T> = T extends (infer U)[] ? U:T;
+
+class ArrayBinder<T extends unknown[]> extends Binder<T>{
     public constructor(
-        protected data:Array<T>,
+        protected data:T,
         protected path:string[],
     ){
         super(data,path);
     }
 
-    public index(index:number){
+    public index(index:number):AllBinderChecker<ElementType<T>>{
         return createBinderWithPath(this.data[index],[...this.path,index+""]);
     }
 }
--- typescript/generic/src/index.ts
+++ typescript/generic/src/index.ts
@@ -6,6 +6,12 @@ type User = {
     name:string
 }
 
+type Country = {
+    id:number,
+    name:string,
+    users:User[]
+}
+
 function createObjectBinder(){
     //使用对象类型
     let user:User = {
@@ -15,16 +21,19 @@ function createObjectBinder(){
 
     //使用工厂方法生成class
     let binder = createBinder(user);
-    console.log(binder);
-
-    console.log(binder.get());
+   
+    //ts推导出来binder为ObjectBinder类型，有key方法
     let binder2 = binder.key("userId");
-    let binder3 = binder2.key("cc");
+    //所以ts能推导出来，它没有index方法
+    //console.log(binder.index(0));
+
+    //最后ts能推导出来binder2是NumberBinder类型
+    console.log(binder2.get());
 }
 
 function createArrayBinder(){
     //使用对象类型
-    let user:User[] = [{
+    let users:User[] = [{
         userId:1,
         name:"fish",
     },{
@@ -33,9 +42,72 @@ function createArrayBinder(){
     }];
 
     //使用工厂方法生成class
-    let binder = createBinder(user);
-    console.log(binder);
+    let binder = createBinder(users);
+
+    //ts推导出来binder为ArrayBinder类型，有index方法
+    let binder2 = binder.index(0);
+    //所以ts能推导出来，它没有key方法
+    //console.log(binder.key("mm"));
+
+    //ts推导出来binder2为ObjectBinder类型，有key方法，注意，这里是递归调用
+    let binder3 = binder2.key("name");
+    //所以ts推导出来，它没有index方法
+    //console.log(binder2.index(0));
+}
+
+function createComplexBinder(){
+    let countries:Country[] = [
+        {
+            id:1,
+            name:"CN",
+            users:[
+                {
+                    userId:10001,
+                    name:"fish",
+                },
+                {
+                    userId:10002,
+                    name:"cat",
+                }
+            ]
+        },
+        {
+            id:2,
+            name:"UK",
+            users:[
+                {
+                    userId:10003,
+                    name:"mm",
+                },
+                {
+                    userId:10004,
+                    name:"gg",
+                }
+            ]
+        },
+    ];
+
+    let binder = createBinder(countries);
+
+    //正常使用
+    console.log( binder.index(0).key("id").get()) ;
+    console.log( binder.index(0).key("name").get()) ;
+
+    //以下2行会被ts自动报错
+    //console.log( binder.key("name").get()) ;
+    //console.log( binder.index(0).key("name").index(0).get()) ;
+
+    //正常使用
+    console.log( binder.index(0).key("users").index(0).get()) ;
+    console.log( binder.index(0).key("users").index(0).key("userId").get()) ;
+
+    //以下3行会被ts自动报错
+    //console.log( binder.index(0).key("user").index(0).get()) ;
+    //console.log( binder.index(0).key("users").key("userId").get()) ;
+    //console.log( binder.index(0).key("users").index(1).key("userId").index(1).get()) ;
 }
 
 
 createObjectBinder(); 
+createArrayBinder();
+createComplexBinder();
