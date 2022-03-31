5215fc94a62d9d6ba7f9b5e3a8653986dc3967d3
fishedee <306766045@qq.com>
2022-03-31 17:08:57 +0800

    更新swagger到oas接口

--- springboot/swagger/src/main/java/spring_test/SwaggerConfiguration.java
+++ springboot/swagger/src/main/java/spring_test/SwaggerConfiguration.java
@@ -1,18 +1,20 @@
 package spring_test;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 @Profile("development")
 public class SwaggerConfiguration {
     //api接口包扫描路径
@@ -21,7 +23,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
@@ -29,8 +31,9 @@ public class SwaggerConfiguration {
                 .build();
     }
 
-    //http://localhost:8080/swagger-ui.html
+    //http://localhost:8080/swagger-ui/
     //http://localhost:8080/v2/api-docs
+    //http://localhost:8080/v3/api-docs
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("测试服务") //设置文档的标题
