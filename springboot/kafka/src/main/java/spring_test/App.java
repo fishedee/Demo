2192a1adc09906f861f3b25998073645e8d52da6
fishedee <306766045@qq.com>
2023-07-18 22:15:17 +0800

    更新kafka

--- springboot/kafka/src/main/java/spring_test/App.java
+++ springboot/kafka/src/main/java/spring_test/App.java
@@ -1,6 +1,9 @@
 package spring_test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -10,6 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * Hello world!
@@ -23,4 +30,16 @@ public class App
     {
         SpringApplication.run(App.class,args);
     }
+
+    @PostConstruct
+    public void init(){
+        //删除topic4下的关于defaultConsumerGroup的offset
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        AdminClient adminClient = AdminClient.create(properties);
+        Set<TopicPartition> partitionSet = new HashSet<>();
+        partitionSet.add(new TopicPartition("topic4",(short)0));
+        adminClient.deleteConsumerGroupOffsets("defaultConsumerGroup",partitionSet);
+    }
+
 }
