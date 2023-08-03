d4608e269e371c829da8b4c2b8c3fe416e1f3f2e
fishedee <306766045@qq.com>
2023-08-03 21:45:05 +0800

    加入mymono2

---
+++ umi/mymono2/packages/myapp/src/layouts/index.tsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from 'umi';
+import styles from './index.less';
+import {MainPanel} from 'mylib/dist/MainPanel';
+
+export default function Layout() {
+  return (
+    <div className={styles.navs}>
+      <ul>
+        <li>
+          <Link to="/">sdHome</Link>
+        </li>
+        <li>
+          <Link to="/docs">Docs</Link>
+        </li>
+        <li>
+          <a href="https://github.com/umijs/umi">Github</a>
+        </li>
+      </ul>
+      <MainPanel title={"fish"}/>
+      <Outlet />
+    </div>
+  );
+}
---
+++ umi/mymono2/packages/mylib/README.md
@@ -0,0 +1,23 @@
+# 
+
+[![NPM version](https://img.shields.io/npm/v/.svg?style=flat)](https://npmjs.org/package/)
+[![NPM downloads](http://img.shields.io/npm/dm/.svg?style=flat)](https://npmjs.org/package/)
+
+## Install
+
+```bash
+$ pnpm install
+```
+
+```bash
+$ npm run dev
+$ npm run build
+```
+
+## Options
+
+TODO
+
+## LICENSE
+
+MIT
